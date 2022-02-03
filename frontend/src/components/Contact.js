76a1b6d01549272f3bba0bafe5e80f1d08b9415c
kkvanonymous <ninja.kunal01@gmail.com>
2022-02-03 14:00:07 +0530

    add: send email integration
    Signed-off-by: kkvanonymous <ninja.kunal01@gmail.com>

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,13 +1,20 @@
+import { useState } from 'react'
+import emailjs from '@emailjs/browser'
 import styled from 'styled-components'
 import ContactImage from '../images/ContactImage.png'
+import Spinner from './Spinner'
 
 const Container = styled.div`
-  margin-top: 10vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: start;
-  height: 90vh;
+  //margin-top: 10vh;
+  //display: flex;
+  //flex-direction: column;
+  //justify-content: start;
+  //height: 90vh;
+  //height: calc(100vh - 70px);
   width: 100%;
+  //padding: 0px 75px 0px;
+  padding: 0 75px;
+  margin-bottom: 50px;
   background-color: #fff;
 
   @media (max-width: 767px) {
@@ -32,8 +39,8 @@ const InnerContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  //background-color: grey;
-  margin-bottom: 50px;
+  //background-color: #f8f8fa;
+  //margin-bottom: 50px;
 
   @media (max-width: 1000px) {
     //align-items: start;
@@ -42,17 +49,17 @@ const InnerContainer = styled.div`
 const InnerContentContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
+  align-items: center;
   justify-content: center;
   //background-color: yellow;
-  height: 70%;
-  width: 50%;
-  padding-left: 80px;
+  //height: 70%;
+  //width: 50%;
+  //padding-left: 80px;
 
   @media (max-width: 1000px) {
     height: 100%;
     width: 100%;
-    padding-left: 30px;
+    padding-left: 0;
   }
 `
 
@@ -64,34 +71,35 @@ const Image = styled.img`
   }
 `
 
-const Title = styled.h1`
+const Title = styled.p`
   font-family: 'Poppins';
+  font-weight: 800;
+  width: 100%;
   color: #496bf2;
   font-size: 3.5rem;
-  margin-bottom: 0;
   //background-color: pink;
 
   @media (max-width: 1000px) {
-    font-size: 2.5rem;
+    font-size: 2.2rem;
   }
 `
-const SloganContainer = styled.div`
+const SubHeadingContainer = styled.div`
   width: 100%;
   //background-color: green;
 `
 
-const Slogan = styled.h3`
+const SubHeading = styled.p`
   font-family: 'Montserrat';
   font-weight: 500;
   color: #54586a;
-  font-size: 1rem;
-  margin: 0 0 45px 0;
+  font-size: 0.9rem;
+  margin-bottom: 30px;
 `
 
 const Form = styled.form`
-  width: 85%;
-  margin-bottom: 50px;
-  //background-color: pink;
+  width: 100%;
+  //margin-bottom: 50px;
+  //background-color: orange;
 `
 
 const FormLabel = styled.h3`
@@ -106,11 +114,8 @@ const FormInputContainer = styled.div`
   flex-direction: column;
   justify-content: start;
   align-items: start;
-  margin-bottom: 30px;
-
-  &.rightMargin {
-    margin-right: 10px;
-  }
+  //background-color: cyan;
+  margin-bottom: 25px;
 `
 
 const FormInput = styled.input`
@@ -123,16 +128,15 @@ const FormInput = styled.input`
   font-family: 'Quicksand';
   font-weight: 500;
   font-size: 15px;
+`
 
-  ::-webkit-outer-spin-button,
-  ::-webkit-inner-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
-  }
-
-   {
-    -moz-appearance: textfield;
-  }
+const FormTextarea = styled.textarea`
+  width: 100%;
+  outline: none;
+  border: 3px solid #2dd6c1;
+  font-family: 'Quicksand';
+  font-weight: 500;
+  font-size: 15px;
 `
 
 const FormSubmitButton = styled.button`
@@ -146,48 +150,127 @@ const FormSubmitButton = styled.button`
   padding: 10px 20px;
   border: none;
   outline: none;
-  margin-top: ${({ errorMessage }) => (errorMessage ? '5px' : '20px')};
+  margin-top: ${({ errorMessage }) => (errorMessage ? '5px' : '0')};
 `
 
-const FormTextarea = styled.textarea`
-  width: 100%;
-  outline: none;
-  border: 3px solid #2dd6c1;
-  font-family: 'Quicksand';
-  font-weight: 500;
-  font-size: 15px;
+const ErrorMessage = styled.h6`
+  color: red;
+  font-style: italic;
 `
 
 const Contact = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [subject, setSubject] = useState('')
+  const [message, setMessage] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setLoading(true)
+    setErrorMessage('')
+    if (name === '' || email === '' || subject === '' || message === '') {
+      setErrorMessage('Fill all fields')
+      setLoading(false)
+      return
+    }
+
+    if (
+      !/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        email
+      )
+    ) {
+      setErrorMessage('Please give valid email')
+      setLoading(false)
+      return
+    }
+    setErrorMessage('')
+
+    var mailConfig = {
+      name,
+      email,
+      subject,
+      message,
+    }
+
+    emailjs
+      .send(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        mailConfig,
+        process.env.REACT_APP_EMAILJS_USER_ID
+      )
+      .then(
+        (result) => {
+          setLoading(false)
+          alert('Message sent successfully')
+          setName('')
+          setEmail('')
+          setSubject('')
+          setMessage('')
+        },
+        (error) => {
+          setLoading(false)
+          alert('Some error occurred')
+          setName('')
+          setEmail('')
+          setSubject('')
+          setMessage('')
+        }
+      )
+  }
+
   return (
     <Container>
       <InnerContainer>
         <Image src={ContactImage} alt='Contact Image' />
         <InnerContentContainer>
           <Title>CONTACT US!!</Title>
-          <SloganContainer>
-            <Slogan>
+          <SubHeadingContainer>
+            <SubHeading>
               Any questions, remarks or queries? Just write us a message!
-            </Slogan>
-          </SloganContainer>
-          <Form>
+            </SubHeading>
+          </SubHeadingContainer>
+          <Form onSubmit={handleSubmit}>
             <FormInputContainer>
               <FormLabel>NAME</FormLabel>
-              <FormInput type='text' placeholder='Enter Name' />
+              <FormInput
+                type='text'
+                placeholder='Enter name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </FormInputContainer>
             <FormInputContainer>
               <FormLabel>EMAIL ADDRESS</FormLabel>
-              <FormInput type='email' placeholder='Enter Email' />
+              <FormInput
+                type='email'
+                placeholder='Enter email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </FormInputContainer>
             <FormInputContainer className='rightMargin'>
               <FormLabel>SUBJECT</FormLabel>
-              <FormInput type='text' placeholder='Subject' />
+              <FormInput
+                type='text'
+                placeholder='Enter subject'
+                value={subject}
+                onChange={(e) => setSubject(e.target.value)}
+              />
             </FormInputContainer>
             <FormInputContainer>
               <FormLabel>MESSAGE</FormLabel>
-              <FormTextarea />
+              <FormTextarea
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
             </FormInputContainer>
-            <FormSubmitButton type='submit'>SUBMIT</FormSubmitButton>
+            {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+            <FormSubmitButton type='submit' errorMessage={errorMessage}>
+              {loading ? <Spinner width={18} height={18} /> : 'Send Message'}
+            </FormSubmitButton>
           </Form>
         </InnerContentContainer>
       </InnerContainer>
