6cf7253aac0926160d6a3abe50415ac715345731
kkvanonymous <ninja.kunal01@gmail.com>
2022-02-06 11:11:48 +0530

    add: MONGODB_URI atlas
    Signed-off-by: kkvanonymous <ninja.kunal01@gmail.com>

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URL, {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
