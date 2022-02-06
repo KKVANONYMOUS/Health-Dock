4fa10ef7c385136708647b36fe9adf6d1653f2bb
kkvanonymous <ninja.kunal01@gmail.com>
2022-02-06 14:54:00 +0530

    config: deployment
    Signed-off-by: kkvanonymous <ninja.kunal01@gmail.com>

--- backend/server.js
+++ backend/server.js
@@ -26,9 +26,19 @@ app.use('/api/patient', patientRoutes)
 app.use('/api/hospital', hospitalRoutes)
 app.use('/api/upload', uploadRoutes)
 
-app.get('/', (req, res) => {
-  res.send('App is running...')
-})
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 
 app.use(notFoundHandler)
 app.use(errorHandler)
