2c8a4779c9d81eae014411b0fc71d02d3ed2c52f
kkvanonymous <ninja.kunal01@gmail.com>
2022-02-06 13:09:07 +0530

    delete: unused staic directory initialization
    Signed-off-by: kkvanonymous <ninja.kunal01@gmail.com>

--- backend/server.js
+++ backend/server.js
@@ -26,9 +26,6 @@ app.use('/api/patient', patientRoutes)
 app.use('/api/hospital', hospitalRoutes)
 app.use('/api/upload', uploadRoutes)
 
-const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
-
 app.get('/', (req, res) => {
   res.send('App is running...')
 })
